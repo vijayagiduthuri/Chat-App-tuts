4821f0e00dea33b73892716f53c225eb4629c9db
Vijaya Giduthuri <vijayagiduthuri2@gmail.com>
2025-06-19 13:23:27 +0530

    done

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,8 +6,9 @@ const router = express.Router();
 
 router.get('/users',protectRoute,getUserForSidebar);
 
+router.post("/send/:id",protectRoute,sendMessage)
+
 router.get("/:id",protectRoute,getMessages);
 
-router.post("/send/:id",protectRoute,sendMessage)
 
 export default router;
